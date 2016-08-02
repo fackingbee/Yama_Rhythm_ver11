aa97c5024bf017fca6be28e23b218ea8e4eabc9f
fackingbee <fackingbee1983@yahoo.co.jp>
2016-08-02 14:42:09 +0900

    Yama_Rhythm_ver11_13

---
+++ Assets/Yama_Scripts/Novel/UtageShake.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+using Utage;
+
+public class UtageShake : MonoBehaviour {
+
+	// エクセルシートのパラメータを取得する為の変数
+	public AdvEngine engine;
+
+	// 背景格納変数
+	private GameObject bg;
+
+	//揺れの範囲を格納する変数
+	public float degree;
+
+
+
+	void Start () {
+
+		// 揺れ幅の初期化
+		degree = 0.08f;
+
+	}
+		
+
+	void Update () {
+
+		// 再生後は基本bgが存在するので、一度しか進入しない
+		if ( bg==null ){
+			
+			Debug.Log ("最初に背景を取得する");
+
+			bg = GameObject.Find("Bg Default");
+		}
+	}
+
+
+	public void Shake(){
+
+		// "DamageEffect"がTrueならif内に入り、エフェクト実行
+		if((bool)engine.Param.GetParameter ("Shake")){
+
+			Debug.Log ("ShakeはTrueです");
+
+			// iTweenで揺らす
+			iTween.ShakePosition (gameObject, iTween.Hash(
+				"x",degree,
+				"y",degree,
+				"islocal",true,
+				"time",1.5f
+			));
+
+			// エクセルシートのフラグ管理をスクリプト側から操作
+			engine.Param.TrySetParameter("Shake",false);
+
+		}
+	}
+}
